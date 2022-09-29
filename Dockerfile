c156d0073d9a6b8713a04524b3178083af0d0066
Jan Buchar <Teyras@gmail.com>
2022-09-29 10:33:35 +0200

    Prometheus docker discovery service

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.10.4-slim
+
+WORKDIR /app
+EXPOSE 8000
+
+COPY ./requirements.txt /app/requirements.txt
+RUN pip install -r /app/requirements.txt --no-cache-dir
+
+COPY ./ /app
+
+CMD ["uvicorn", "--host=0.0.0.0", "--port=8000", "prometheus_docker_discovery:app"]
---
+++ prometheus_docker_discovery.py
@@ -0,0 +1,49 @@
+from typing import cast
+
+from docker import DockerClient
+from docker.models.containers import Container
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+DISCOVERY_LABEL_PREFIX = "prometheus."
+TARGET_LABELS_PREFIX = f"{DISCOVERY_LABEL_PREFIX}labels."
+TARGET_ADDRESS_LABEL = f"{DISCOVERY_LABEL_PREFIX}address"
+
+
+app = FastAPI()
+client = DockerClient.from_env()
+
+
+class DiscoveredTarget(BaseModel):
+    targets: list[str]
+    labels: dict[str, str]
+
+
+@app.get("/targets", response_model=list[DiscoveredTarget])
+def get_targets():
+    containers = cast(list[Container], client.containers.list(all=True))
+    result = list[DiscoveredTarget]()
+
+    for container in containers:
+        job_name = container.labels.get(f"{DISCOVERY_LABEL_PREFIX}job")
+        if job_name is None:
+            continue
+
+        if not TARGET_ADDRESS_LABEL in container.labels:
+            continue
+
+        target = DiscoveredTarget(
+            targets=[container.labels[TARGET_ADDRESS_LABEL]],
+            labels={"job": job_name},
+        )
+
+        result.append(target)
+
+        for label_name, label_value in container.labels.items():
+            if not label_name.startswith(TARGET_LABELS_PREFIX):
+                continue
+
+            label_name = label_name[len(TARGET_LABELS_PREFIX) :]
+            target.labels[label_name] = label_value
+
+    return result
